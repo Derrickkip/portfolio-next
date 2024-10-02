3654129fff4bde05e93ab703773e28adf45d9719
derrickkip <derrick.korir@gmail.com>
2024-10-02 17:34:47 +0300

    Adjust homescreen for responsiveness

---
+++ app/components/Button.js
@@ -0,0 +1,7 @@
+export default function Button ({text}) {
+    return (
+        <button className="border border-white px-8 py-2 text-sm md:text-lg uppercase">
+            {text}
+        </button>
+    )
+}
--- app/page.js
+++ app/page.js
@@ -1,66 +1,24 @@
 import Image from "next/image";
 
+import Button from "./components/Button";
+
 const profPic = require('./images/derrick.jpg')
 
 export default function Home() {
     return (
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-space-mono)]">
-            <main className="flex flex-col md:flex-row md:gap-8 row-start-2 items-center">
-                    <div className="w-full h-full md:order-1">
-                        <Image src={profPic} alt="profile picture" width={200} height={200} className="w-full h-full rounded-full"/>
-                    </div> 
-                    <div className="w-full h-full">
-                        <h3 className="text-sm md:text-lg text-gray-500 my-2">&lt;Hello, world&gt;</h3>
-                        <p className="text-2xl md:text-4xl">My name is Derrick Korir. I am a fullstack software developer from Nairobi.</p>
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-space-mono)]">
+            <main className="flex flex-col md:flex-row lg:justify-between md:gap-8 row-start-2 items-center">
+                <div className="w-full h-full md:order-1 pb-8 lg:pb-0">
+                    <Image src={profPic} alt="profile picture" width={200} height={200} className="w-64 h-full md:w-96 mx-auto rounded-full" />
+                </div>
+                <div className="w-full h-full">
+                    <h3 className="text-sm md:text-lg text-gray-500 my-2">&lt;Hello, world&gt;</h3>
+                    <p className="text-xl md:text-2xl lg:text-4xl">My name is Derrick Korir. I am a fullstack software developer from Nairobi.</p>
+                    <div className="mt-8 lg:mt-16">
+                        <Button text={'Contact me'} />
                     </div>
+                </div>
             </main>
-            {/* <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer> */}
         </div>
     );
 }
