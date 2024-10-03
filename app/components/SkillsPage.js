e92d18b8f6167c04caa9b2fbd2ae33a8a2016ace
derrickkip <derrick.korir@gmail.com>
2024-10-03 17:26:55 +0300

    Add Skills page

--- app/components/SkillsPage.js
+++ app/components/SkillsPage.js
@@ -1,13 +1,20 @@
 
 export default function Skills() {
     return (
-        <div className="grid grid-rows-[20px_1fr_20px] min-h-screen  min-h-screen p-8 pb-20 sm:p-20">
+        <div className="grid grid-rows-[30px_1fr_1fr] min-h-screen  min-h-screen p-8 pb-20 sm:p-20">
             <div>
                 <h3 className="text-sm md:text-lg text-gray-500">&lt;Skills&gt;</h3>
             </div>
             <div className="flex justify-between w-full">
                 <div className="w-1/2">
-                    <p>Some description</p>
+                    <h2 className="text-4xl font-bold">Skills I possess as a specialist</h2>
+                    <ul className="list-disc p-6 text-sm">
+                        <li>Website development</li>
+                        <li>Database development</li>
+                        <li>Python program development</li>
+                        <li>Website parsing, automization, data filling</li>
+                        <li>Chatbot development, extensive experience in Telegram</li>
+                    </ul>
                 </div>
                 <div className="w-1/2">
                     <p>icons</p>
